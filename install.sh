90a46cff6b3f35cf5b28dc2d88a181ae15b9d066
Sergey Volkov <fenixguard1993@gmail.com>
2021-03-20 17:33:20 +0300

    fixed .service, install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ git clone https://www.github.com/fenixguard/candyshop_api.git
 echo "Done!"
 
 echo "Coping config file..."
-cp /home/entrant/soft/candyshop_api/candyshop.service /etc/systemd/system/
+sudo cp /home/entrant/soft/candyshop_api/candyshop.service /etc/systemd/system/
 echo "Done!"
 
 echo "Reloading daemon..."
-systemctl daemon-reload
+sudo systemctl daemon-reload
 echo "Daemon reloaded!"
 
 echo "Starting service..."
-systemctl start candyshop
+sudo systemctl start candyshop
 echo "Service started on 0.0.0.0:8080 "
